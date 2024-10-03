eb2545e837b567c30673dcb1fb6ff2b6f0fef06e
Blaj3n <benceulrich@gmail.com>
2024-10-03 17:57:10 +0200

    2. Hányszor fizettek a pénztárnál

--- otszaz.py
+++ otszaz.py
@@ -15,3 +15,10 @@ with open("penztar.txt", "r", encoding="utf-8") as file:
 print(targyak)
 
 print("")
+
+print("2. feladat")
+
+szamlalo = 0
+for egykosar in targyak:
+    szamlalo += 1
+print(f"A fizetések száma: {szamlalo}")
